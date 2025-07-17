0a6ca07e0076744974bb0beb9ac771c86c94edec
Oleg Stepanischev <olegxx@gmail.com>
2025-07-17 20:13:18 +0300

    Separate compression validation tests

---
+++ src/ZstdSharp.Test/ZstdValidationTest.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using Zstd.Extern;
+
+namespace ZstdSharp.Test
+{
+    public unsafe class ZstdValidation
+    {
+        private Span<byte> CompressNative(byte[] srcBuffer, int compressBound, int level)
+        {
+            var buffer = new byte[compressBound];
+            fixed (byte* bufferPtr = buffer)
+            fixed (byte* srcPtr = srcBuffer)
+            {
+                var cctx = ExternMethods.ZSTD_createCCtx();
+                try
+                {
+                    ExternMethods.ZSTD_CCtx_setParameter(cctx, ExternMethods.ZSTD_cParameter.ZSTD_c_compressionLevel,
+                        level);
+
+                    var length = ExternMethods.ZSTD_compress2(cctx,
+                        (IntPtr) bufferPtr, (nuint) buffer.Length,
+                        (IntPtr) srcPtr, (nuint) srcBuffer.Length);
+                    return new Span<byte>(buffer, 0, (int) length);
+                }
+                finally
+                {
+
+                    ExternMethods.ZSTD_freeCCtx(cctx);
+                }
+            }
+        }
+
+        private Span<byte> DecompressNative(ReadOnlySpan<byte> src, int decompressBound)
+        {
+            fixed (byte* srcPtr = src)
+            {
+                var buffer = new byte[decompressBound];
+                fixed (byte* decompressedBufferNativePtr = buffer)
+                {
+                    var dctx = ExternMethods.ZSTD_createDCtx();
+                    try
+                    {
+                        var length = ExternMethods.ZSTD_decompressDCtx(dctx,
+                            (IntPtr) decompressedBufferNativePtr,
+                            (nuint) buffer.Length,
+                            (IntPtr) srcPtr, (nuint) src.Length);
+                        return new Span<byte>(buffer, 0, (int) length);
+                    }
+                    finally
+                    {
+
+                        ExternMethods.ZSTD_freeDCtx(dctx);
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<object[]> LevelsData =>
+            Enumerable.Range(-5, 5)
+                .Concat(Enumerable.Range(1, Compressor.MaxCompressionLevel))
+                .Select(level => new object[] {level});
+
+        [Theory]
+        [MemberData(nameof(LevelsData))]
+        public void CompressAndDecompressWithNative(int level)
+        {
+            var srcBuffer = File.ReadAllBytes("dickens");
+
+            var compressBound = Compressor.GetCompressBound(srcBuffer.Length);
+            Assert.Equal((int) ExternMethods.ZSTD_compressBound((nuint) srcBuffer.Length), compressBound);
+
+            using var compressor = new Compressor(level);
+            var compressedSharp = compressor.Wrap(srcBuffer);
+            var compressedNative = CompressNative(srcBuffer, compressBound, level);
+            Assert.True(compressedNative.SequenceEqual(compressedSharp));
+
+            var decompressBound = (int) Decompressor.GetDecompressedSize(compressedSharp);
+            Assert.Equal(decompressBound, srcBuffer.Length);
+
+            using var decompressor = new Decompressor();
+            var decompressedSharp = decompressor.Unwrap(compressedSharp);
+            var decompressedNative = DecompressNative(compressedNative, decompressBound);
+            Assert.True(decompressedSharp.SequenceEqual(decompressedNative));
+        }
+    }
+}
